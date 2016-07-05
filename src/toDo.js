26b50ba01fffabaeac2b9bdba0fb8e4333f371a9
Marlon Date-Chong <mdatechong@gmail.com>
2016-07-05 10:51:04 +0100

    returned list as HTML

--- src/toDo.js
+++ src/toDo.js
@@ -2,16 +2,23 @@
 
 function ToDo(){
   this.tasks = [];
-
+  this.html =["<ul>"]
 };
-
-
-var toDo = new ToDo();
 
 ToDo.prototype.tasks = function() {
   return this.tasks;
 };
 
+ToDo.prototype.add = function (task) {
+  this.tasks.push(task);
+};
 
+ToDo.prototype.returnList = function () {
+  for(var i = 0; i < this.tasks.length; i++) {
+    this.html.push("<li>" + this.tasks[i] + "</li>");
+  }
+  this.html.push("</ul>");
+  return this.html.join('');
+};
 
 module.exports=ToDo;
--- test/toDoTest.js
+++ test/toDoTest.js
@@ -12,10 +12,21 @@ describe('create a to-do', function(){
   it('can instantiate a to-do object',function(){
       expect(toDo).to.be.an.instanceof(ToDo);
     })
-    
+
   it('can create a to-do list',function(){
     expect(toDo.tasks).to.be.empty;
+  })
 
+  it('can add a task to the to-do list', function() {
+    toDo.add('Clean Room');
+    expect(toDo.tasks).to.include('Clean Room');
+    toDo.tasks = []
+  })
+
+  it('returns an HTML unordered list', function() {
+    toDo.add('Clean Room');
+    toDo.add('Buy Milk')
+    expect(toDo.returnList()).to.eql("<ul><li>Clean Room</li><li>Buy Milk</li></ul>");
   })
 
 })
