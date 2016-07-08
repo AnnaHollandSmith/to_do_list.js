7f75ce717dfc2d081c37d7d615e0c7ef717ddbb8
lomlo <michael.lo@live.co.uk>
2016-07-08 10:36:32 +0100

    added a textbox reset method

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ function addTask(){
   var newItem = document.getElementById("task_input").value
   new_list.addItem(newItem);
   document.getElementById('list').innerHTML = new_list.toHtml();
+  resetContent();
 }
 
 function complete(i){
   new_list._list[i].finish();
   var task = document.getElementById("list_"+i);
   task.className += "completed";
+}
+
+function resetContent(){
+  var inputBox = document.getElementById("task_input");
+  inputBox.value='';
 }
